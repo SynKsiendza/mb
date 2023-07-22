ec725e5d20529c3b9490502b18a63fa3546e3677
SynKsiendza <synksiendza@proton.me>
2023-07-22 21:21:52 +0200

    1.1.0.0
    Dodane:
     - Strona "Artykuły Malarskie"
     - 1 Przedmiot (Bez opisu i nazwy)

---
+++ js/more-info-farby.js
@@ -0,0 +1,17 @@
+
+function pendzel()
+{
+    document.getElementById('dist-pendzel').innerHTML = "<br><br><br><br><br>";
+    document.getElementById('moreinfo-pendzel').style.cssText ="" +
+    "width: 60%;min-height: 100px;background-color: rgba(112, 112, 112, 0.90);padding: 5px;border-radius: 15px;border-color: rgba(204, 0, 0, 0.70);border-style: solid;border-width: 2px;margin-top: 1%;margin-bottom: auto;font-size: 80%;margin-left: auto;margin-right: 5%;float: right; position: relative;";
+    
+    document.getElementById("moreinfo-pendzel").innerHTML = ""+
+    "<img style='float: left;' src='img/right-arrow.png' width='100' height='100'>" +
+    "<p style='width: 70%; float: left; min-height: 20px;'>"+ 
+    "Pendzelek ogółem, całe te <br>"+
+    "Pendzelek ogółem, całe te <br>" +
+    "Pendzelek ogółem, całe te <br>" +
+    "Pendzelek ogółem, całe te <br>" +
+    "</p>";
+
+}    
