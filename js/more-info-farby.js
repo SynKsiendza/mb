8e5eb5ba309805b218e54fb4ef67501664d2be4b
SynKsiendza <synksiendza@proton.me>
2023-07-22 22:09:32 +0200

    1.2.0

--- js/more-info-farby.js
+++ js/more-info-farby.js
@@ -1,17 +1,62 @@
-
-function pendzel()
+function plamoodporna()
 {
-    document.getElementById('dist-pendzel').innerHTML = "<br><br><br><br><br>";
-    document.getElementById('moreinfo-pendzel').style.cssText ="" +
+    document.getElementById('dist-plamoodporna').innerHTML = "<br><br><br><br><br>";
+    document.getElementById('moreinfo-plamoodporna').style.cssText ="" +
     "width: 60%;min-height: 100px;background-color: rgba(112, 112, 112, 0.90);padding: 5px;border-radius: 15px;border-color: rgba(204, 0, 0, 0.70);border-style: solid;border-width: 2px;margin-top: 1%;margin-bottom: auto;font-size: 80%;margin-left: auto;margin-right: 5%;float: right; position: relative;";
     
-    document.getElementById("moreinfo-pendzel").innerHTML = ""+
+    document.getElementById("moreinfo-plamoodporna").innerHTML = ""+
     "<img style='float: left;' src='img/right-arrow.png' width='100' height='100'>" +
-    "<p style='width: 70%; float: left; min-height: 20px;'>"+ 
-    "Pendzelek ogółem, całe te <br>"+
-    "Pendzelek ogółem, całe te <br>" +
-    "Pendzelek ogółem, całe te <br>" +
-    "Pendzelek ogółem, całe te <br>" +
+    "<p style='width: 80%; float: left; min-height: 20px;'>"+ 
+    "<b>Śnieżka Plamoodporna</b> to matowa lateksowa farba do wnętrz przeznaczona do malowania ścian wewnętrznych i sufitów. Jest odporna na plamy, zmywanie i szorowanie zachowując przy tym idealny wygląd i głęboko matowy kolor ścian. Farba nie chlapie podczas malowania, jest łatwa w aplikacji i doskonale kryje.<br>"+
+    "</p>";
+
+}
+function bn()
+{
+    document.getElementById('dist-bn').innerHTML = "<br><br><br><br><br><br><br><br>";
+    document.getElementById('moreinfo-bn').style.cssText ="" +
+    "width: 60%;min-height: 100px;background-color: rgba(112, 112, 112, 0.90);padding: 5px;border-radius: 15px;border-color: rgba(204, 0, 0, 0.70);border-style: solid;border-width: 2px;margin-top: 1%;margin-bottom: auto;font-size: 80%;margin-left: auto;margin-right: 5%;float: right; position: relative;";
+    
+    document.getElementById("moreinfo-bn").innerHTML = ""+
+    "<img style='float: left;' src='img/right-arrow.png' width='100' height='100'>" +
+    "<p style='width: 80%; float: left; min-height: 20px;'>"+ 
+    "Wysokiej jakości emulsyjna farba lateksowa charakteryzująca się dużą siłą krycia, bardzo wysoką wydajnością i trwałością kolorów powłoki malarskiej. Daje matowe i w pełni pozwalające „oddychać ścianom” powłoki malarskie odporne na szorowanie. Przeznaczona jest do ochronno-dekoracyjnego malowania tynków wapiennych i cementowo-wapiennych, podłoży cementowych, betonowych, gipsowych, drewnianych i drewnopochodnych, cegły, kamienia i płyt gipsowo-kartonowych użytkowanych wewnątrz pomieszczeń.<br>"+
     "</p>";
 
 }    
+function Eko()
+{
+    document.getElementById('dist-Eko').innerHTML = "<br><br><br><br><br><br>";
+    document.getElementById('moreinfo-Eko').style.cssText ="" +
+    "width: 60%;min-height: 100px;background-color: rgba(112, 112, 112, 0.90);padding: 5px;border-radius: 15px;border-color: rgba(204, 0, 0, 0.70);border-style: solid;border-width: 2px;margin-top: 1%;margin-bottom: auto;font-size: 80%;margin-left: auto;margin-right: 5%;float: right; position: relative;";
+    
+    document.getElementById("moreinfo-Eko").innerHTML = ""+
+    "<img style='float: left;' src='img/right-arrow.png' width='100' height='100'>" +
+    "<p style='width: 80%; float: left; min-height: 20px;'>"+ 
+    "Farba przeznaczona jest do dekoracyjnego malowania ścian i sufitów z zapraw cementowych, cementowo-wapiennych, wapiennych, gipsowych, płyt gipsowo-kartonowych, drewnianych i z materiałów drewnopochodnych wewnątrz pomieszczeń. Daje matowe, śnieżnobiałe i w pełni pozwalające „oddychać ścianom” powłoki.<br>"+
+    "</p>";
+}
+function EkoPlus()
+{
+    document.getElementById('dist-EkoPlus').innerHTML = "<br><br><br><br><br><br>";
+    document.getElementById('moreinfo-EkoPlus').style.cssText ="" +
+    "width: 60%;min-height: 100px;background-color: rgba(112, 112, 112, 0.90);padding: 5px;border-radius: 15px;border-color: rgba(204, 0, 0, 0.70);border-style: solid;border-width: 2px;margin-top: 1%;margin-bottom: auto;font-size: 80%;margin-left: auto;margin-right: 5%;float: right; position: relative;";
+    
+    document.getElementById("moreinfo-EkoPlus").innerHTML = ""+
+    "<img style='float: left;' src='img/right-arrow.png' width='100' height='100'>" +
+    "<p style='width: 80%; float: left; min-height: 20px;'>"+ 
+    "Farba przeznaczona jest do dekoracyjnego malowania ścian i sufitów wewnątrz pomieszczeń: z zapraw cementowych, cementowo-wapiennych, wapiennych, gipsowych, płyt gipsowo-kartonowych, drewnianych, drewnopochodnych oraz tapet, również tych z włókna szklanego.<br>"+
+    "</p>";
+}
+function EasyCare()
+{
+    document.getElementById('dist-EasyCare').innerHTML = "<br><br><br><br><br><br>";
+    document.getElementById('moreinfo-EasyCare').style.cssText ="" +
+    "width: 60%;min-height: 100px;background-color: rgba(112, 112, 112, 0.90);padding: 5px;border-radius: 15px;border-color: rgba(204, 0, 0, 0.70);border-style: solid;border-width: 2px;margin-top: 1%;margin-bottom: auto;font-size: 80%;margin-left: auto;margin-right: 5%;float: right; position: relative;";
+    
+    document.getElementById("moreinfo-EasyCare").innerHTML = ""+
+    "<img style='float: left;' src='img/right-arrow.png' width='100' height='100'>" +
+    "<p style='width: 80%; float: left; min-height: 20px;'>"+ 
+    "<b>Dulux EasyCare</b> to pierwsza na rynku plamoodporna farba wewnętrzna do ścian i sufitów o rewolucyjnych właściwościach hydrofobowych. Dzięki specjalnej powłoce farba odpycha wodę oraz wszelkie płynne zabrudzenia nie pozwalając im przeniknąć w głąb powierzchni. Tworzy matowe powłoki o najwyższej odporności na plamy, a ich usuwanie jest dziecinnie łatwe.<br>"+
+    "</p>";
+}
